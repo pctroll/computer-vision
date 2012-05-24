25a57edc2141da9ebf9aad9ca7797f3a9c0bb4dc
pctroll <pctroll@gmail.com>
2012-05-24 07:34:58 -0430

    A little further on Iris' isolation and image normalization Created a mask from the Hough Transform in order to isolate the Iris. Started writing the normalized image (from polar to Cartesian) but the solution is not quite simple (or so it seems). TODO: Create a new issue regarding searching optimal value for param2 for HoughCircles function.

--- iris_recognition/iris_recognition.py
+++ iris_recognition/iris_recognition.py
@@ -3,25 +3,18 @@
 
 import cv
 import math
-import numpy
+import numpy as np
 
 centroid = (0,0)
-pupilArea = 0
-pupil = []
-
-def getRadius(area):
-	r = 1.0
-	r = math.sqrt(area/3.14)
-	return (r)
+radius = 0
+magnitude = 0
 
 def getPupil(frame):
 	pupilImg = cv.CreateImage(cv.GetSize(frame), 8, 1)
 	cv.InRangeS(frame, (30,30,30), (80,80,80), pupilImg)
-	cv.ShowImage("output", pupilImg)
 	contours = cv.FindContours(pupilImg, cv.CreateMemStorage(0), mode = cv.CV_RETR_EXTERNAL)
 	del pupilImg
-	x,y = -10, -10
-	i = 0
+	pupilImg = cv.CloneImage(frame)
 	while contours:
 		moments = cv.Moments(contours)
 		area = cv.GetCentralMoment(moments,0,0)
@@ -30,47 +23,58 @@ def getPupil(frame):
 			x = cv.GetSpatialMoment(moments,1,0)/area
 			y = cv.GetSpatialMoment(moments,0,1)/area
 			pupil = contours
-			point = (int(x),int(y))
 			global centroid
-			centroid = point
-			cv.DrawContours(frame, pupil, (0,0,0), (0,0,0), 2, cv.CV_FILLED)
-			#cv.Circle(frame, (int(x),int(y)), 2, (0,255,0), int(r)*2)
+			centroid = (int(x),int(y))
+			cv.DrawContours(pupilImg, pupil, (0,0,0), (0,0,0), 2, cv.CV_FILLED)
 			break
 		contours = contours.h_next()
-		i += 1
-	return (frame)
+	return (pupilImg)
 
 def getIris(frame):
+	copyImg = cv.CloneImage(frame)
+	resImg = cv.CloneImage(frame)
 	grayImg = cv.CreateImage(cv.GetSize(frame), 8, 1)
+	mask = cv.CreateImage(cv.GetSize(frame), 8, 1)
 	storage = cv.CreateMat(frame.width, 1, cv.CV_32FC3)
 	cv.CvtColor(frame,grayImg,cv.CV_BGR2GRAY)
 	cv.Canny(grayImg, grayImg, 5, 70, 3)
 	cv.Smooth(grayImg,grayImg,cv.CV_GAUSSIAN, 7, 7)
-	minRad = int(getRadius(pupilArea))
 	cv.HoughCircles(grayImg, storage, cv.CV_HOUGH_GRADIENT, 2, 100.0, 30, 150, 100, 140)
-	#cv.ShowImage("output", grayImg)
-	circles = numpy.asarray(storage)
+	circles = np.asarray(storage)
 	for circle in circles:
 		rad = int(circle[0][2])
-		cv.Circle(frame, centroid, rad, cv.CV_RGB(255,0,0), 1, 8, 0)
-		#cv.DrawContours(frame, circles, (0,0,0), (0,0,0), 2)
-		#print "circle!"
-	return (frame)
+		global radius
+		radius = rad
+		cv.Circle(mask, centroid, rad, cv.CV_RGB(255,255,255), cv.CV_FILLED)
+		cv.Not(mask,mask)
+		cv.Sub(frame,copyImg,resImg,mask)
+	return (resImg)
+	
 
 
 cv.NamedWindow("input", cv.CV_WINDOW_AUTOSIZE)
 cv.NamedWindow("output", cv.CV_WINDOW_AUTOSIZE)
+cv.NamedWindow("normalized", cv.CV_WINDOW_AUTOSIZE)
+
 key = 0
 while True:
-	frame = cv.LoadImageM("images/R/S1001R02.jpg")
+	frame = cv.LoadImage("images/R/S1001R02.jpg")
+	iris = cv.CloneImage(frame)
 	output = getPupil(frame)
-	cv.ShowImage("input", frame)
 	iris = getIris(output)
+	x = float(centroid[0])
+	y = float(centroid[1])
+	cv.ShowImage("input", frame)
 	cv.ShowImage("output", iris)
-	key = cv.WaitKey(500)
+	size = cv.GetSize(frame)
+	size = (int(size[0]), int(size[1]*3.14))
+	normImg = cv.CreateImage(size, 8, 3)
+	cv.LogPolar(iris, normImg, (x,y), 40)
+	cv.ShowImage("normalized", normImg)
+	key = cv.WaitKey(1000)
 	# seems like Esc with NumLck equals 1048603
 	if (key == 27 or key == 1048603):
 		break
 
-cv.DestroyWindow("input")
+cv.DestroyAllWindows()
 
