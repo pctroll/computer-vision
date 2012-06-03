c0782d45240ec7b171a25db228e5806e72b69c50
Jorge Palacios <pctroll@gmail.com>
2012-06-03 10:13:28 -0430

    Code re-structuring and added function comments

--- iris_recognition/iris_recognition.py
+++ iris_recognition/iris_recognition.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python
 # @author	pctroll a.k.a Jorge Palacios 10-87970
 # @file		iris_detection.py
 
@@ -6,12 +7,25 @@ import math
 import numpy as np
 import os
 
+# GLOBAL VARIABLES
+#####################################
+# Holds the pupil's center
 centroid = (0,0)
+# Holds the iris' radius
 radius = 0
-#magnitude = 0
+# Holds the current element of the image used by the getNewEye function
 currentEye = 0
+# Holds the list of eyes (filenames)
 eyesList = []
+#####################################
 
+
+# Returns a different image filename on each call. If there are no more
+# elements in the list of images, the function resets.
+#
+# @param list		List of images (filename)
+# @return string	Next image (filename). Starts over when there are
+#			no more elements in the list.
 def getNewEye(list):
 	global currentEye
 	if (currentEye >= len(list)):
@@ -20,38 +34,13 @@ def getNewEye(list):
 	currentEye += 1
 	return (newEye)
 
-def getCircles(image):
-	i = 80
-	while i < 151:
-		storage = cv.CreateMat(image.width, 1, cv.CV_32FC3)
-		cv.HoughCircles(image, storage, cv.CV_HOUGH_GRADIENT, 2, 100.0, 30, i, 100, 140)
-		circles = np.asarray(storage)
-		if (len(circles) == 1):
-			return circles
-		i +=1
-	return ([])
-
-def getPupil(frame):
-	pupilImg = cv.CreateImage(cv.GetSize(frame), 8, 1)
-	cv.InRangeS(frame, (30,30,30), (80,80,80), pupilImg)
-	contours = cv.FindContours(pupilImg, cv.CreateMemStorage(0), mode = cv.CV_RETR_EXTERNAL)
-	del pupilImg
-	pupilImg = cv.CloneImage(frame)
-	while contours:
-		moments = cv.Moments(contours)
-		area = cv.GetCentralMoment(moments,0,0)
-		if (area > 50):
-			pupilArea = area
-			x = cv.GetSpatialMoment(moments,1,0)/area
-			y = cv.GetSpatialMoment(moments,0,1)/area
-			pupil = contours
-			global centroid
-			centroid = (int(x),int(y))
-			cv.DrawContours(pupilImg, pupil, (0,0,0), (0,0,0), 2, cv.CV_FILLED)
-			break
-		contours = contours.h_next()
-	return (pupilImg)
-
+# Returns the cropped image with the isolated iris and black-painted
+# pupil. It uses the getCircles function in order to look for the best
+# value for each image (eye) and then obtaining the iris radius in order
+# to create the mask and crop.
+#
+# @param image		Image with black-painted pupil
+# @returns image 	Image with isolated iris + black-painted pupil
 def getIris(frame):
 	iris = []
 	copyImg = cv.CloneImage(frame)
@@ -81,13 +70,64 @@ def getIris(frame):
 		cv.ResetImageROI(resImg)
 		return(cropImg)
 	return (resImg)
-	
-def getPolar2CartImg(image, center, rad):
-	c = (float(center[0]), float(center[1]))
-	imgRes = cv.CreateImage((rad*2, int(360)), 8, 3)
-	cv.LogPolar(image,imgRes,c,25.0)
+
+# Search middle to big circles using the Hough Transform function
+# and loop for testing values in the range [80,150]. When a circle is found,
+# it returns a list with the circles' data structure. Otherwise, returns an empty list.
+
+# @param image
+# @returns list
+def getCircles(image):
+	i = 80
+	while i < 151:
+		storage = cv.CreateMat(image.width, 1, cv.CV_32FC3)
+		cv.HoughCircles(image, storage, cv.CV_HOUGH_GRADIENT, 2, 100.0, 30, i, 100, 140)
+		circles = np.asarray(storage)
+		if (len(circles) == 1):
+			return circles
+		i +=1
+	return ([])
+
+# Returns the same images with the pupil masked black and set the global
+# variable centroid according to calculations. It uses the FindContours 
+# function for finding the pupil, given a range of black tones.
+
+# @param image		Original image for testing
+# @returns image	Image with black-painted pupil
+def getPupil(frame):
+	pupilImg = cv.CreateImage(cv.GetSize(frame), 8, 1)
+	cv.InRangeS(frame, (30,30,30), (80,80,80), pupilImg)
+	contours = cv.FindContours(pupilImg, cv.CreateMemStorage(0), mode = cv.CV_RETR_EXTERNAL)
+	del pupilImg
+	pupilImg = cv.CloneImage(frame)
+	while contours:
+		moments = cv.Moments(contours)
+		area = cv.GetCentralMoment(moments,0,0)
+		if (area > 50):
+			pupilArea = area
+			x = cv.GetSpatialMoment(moments,1,0)/area
+			y = cv.GetSpatialMoment(moments,0,1)/area
+			pupil = contours
+			global centroid
+			centroid = (int(x),int(y))
+			cv.DrawContours(pupilImg, pupil, (0,0,0), (0,0,0), 2, cv.CV_FILLED)
+			break
+		contours = contours.h_next()
+	return (pupilImg)
+
+# Returns the image as a "tape" converting polar coord. to Cartesian coord.
+#
+# @param image		Image with iris and pupil
+# @returns image	"Normalized" image
+def getPolar2CartImg(image, rad):
+	imgSize = cv.GetSize(image)
+	c = (float(imgSize[0]/2.0), float(imgSize[1]/2.0))
+	imgRes = cv.CreateImage((rad*3, int(360)), 8, 3)
+	#cv.LogPolar(image,imgRes,c,50.0, cv.CV_INTER_LINEAR+cv.CV_WARP_FILL_OUTLIERS)
+	cv.LogPolar(image,imgRes,c,60.0, cv.CV_INTER_LINEAR+cv.CV_WARP_FILL_OUTLIERS)
 	return (imgRes)
 
+# Window creation for showing input, output
 cv.NamedWindow("input", cv.CV_WINDOW_AUTOSIZE)
 cv.NamedWindow("output", cv.CV_WINDOW_AUTOSIZE)
 cv.NamedWindow("normalized", cv.CV_WINDOW_AUTOSIZE)
@@ -103,7 +143,7 @@ while True:
 	cv.ShowImage("input", frame)
 	cv.ShowImage("output", iris)
 	normImg = cv.CloneImage(iris)
-	normImg = getPolar2CartImg(iris,centroid,radius)
+	normImg = getPolar2CartImg(iris,radius)
 	cv.ShowImage("normalized", normImg)
 	key = cv.WaitKey(3000)
 	# seems like Esc with NumLck equals 1048603
